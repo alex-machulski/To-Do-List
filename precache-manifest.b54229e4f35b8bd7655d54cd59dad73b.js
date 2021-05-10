14c2935abf257ebcce0410e6394e80e758111385
alex-machulski <aliaksandr.machulski@gmail.com>
2021-05-10 19:56:36 +0300

    Updates

---
+++ precache-manifest.b54229e4f35b8bd7655d54cd59dad73b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ff1911ba7a2dd19b055486367e6e95",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "365831bda747855a6b7c",
+    "url": "/To-Do-List/static/css/main.b7023c28.chunk.css"
+  },
+  {
+    "revision": "c2d3cf8739cb46fbc1d3",
+    "url": "/To-Do-List/static/js/2.5efb00ef.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/To-Do-List/static/js/2.5efb00ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365831bda747855a6b7c",
+    "url": "/To-Do-List/static/js/main.de384006.chunk.js"
+  },
+  {
+    "revision": "a75a09460f2a6dbb7116",
+    "url": "/To-Do-List/static/js/runtime-main.4bd6e5d5.js"
+  }
+]);
